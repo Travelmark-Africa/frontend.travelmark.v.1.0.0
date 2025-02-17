4cab1c219a5dd43e421b4da4095cb1d9991b8fed
John Niyontwali <nijohn0006@gmail.com>
2025-02-17 12:32:53 +0200

    setup done

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className='bg-black text-white h-screen'>
+      <h1>Hello world</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae molestiae facilis maiores eligendi quae. Repellendus provident dolor numquam enim doloribus, fugit exercitationem illo velit laboriosam. Beatae quas esse incidunt voluptatem.</p>
+    </div>
+  );
+};
+
+export default App;
